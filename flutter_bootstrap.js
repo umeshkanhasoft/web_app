aabe6f60e9277d398ee48cabee5ac92195e7d9b0
ashish <ashish@kanhasoft.com>
2024-12-01 16:25:25 +0530

    local changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "838455084"
+    serviceWorkerVersion: "3611551510"
   }
 });
--- lib/web_video_player.dart
+++ lib/web_video_player.dart
@@ -22,8 +22,10 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
   RxInt currentPlayerIndex = 0.obs;
   RxBool videoIsLoaded = false.obs;
   RxString title = "".obs;
-  RxBool isMovie = false.obs;
+  RxBool isMovie = true.obs;
   RxList<Map<String, dynamic>> listOfMovie = <Map<String, dynamic>>[].obs;
+  RxList<Map<String, dynamic>> filteredList = <Map<String, dynamic>>[].obs;
+
   Player player = Player();
   VideoController? controller;
 
@@ -32,6 +34,44 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
     super.initState();
     controller = VideoController(player);
     setUpData();
+  }
+
+  void searchItems(String query) {
+    filteredList.value = listOfMovie.where((item) {
+      return (item['mn'] as String).toLowerCase().contains(query.toLowerCase());
+    }).toList();
+  }
+
+  // Method to highlight the search term in the list item
+  TextSpan highlightText(String text, String query) {
+    List<TextSpan> spans = [];
+    int start = 0;
+    String lowerText = text.toLowerCase();
+    String lowerQuery = query.toLowerCase();
+
+    while (start < text.length) {
+      int index = lowerText.indexOf(lowerQuery, start);
+
+      if (index == -1) {
+        spans.add(TextSpan(text: text.substring(start)));
+        break;
+      }
+
+      // Add text before the match
+      if (index > start) {
+        spans.add(TextSpan(text: text.substring(start, index)));
+      }
+
+      // Highlight the match
+      spans.add(TextSpan(
+        text: text.substring(index, index + query.length),
+        style: const TextStyle(backgroundColor: Colors.yellow),
+      ));
+
+      start = index + query.length;
+    }
+
+    return TextSpan(children: spans);
   }
 
   setUpData() async {
@@ -46,6 +86,7 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
     if (data.containsKey('AllMovieDataList')) {
       for (var e in (data['AllMovieDataList'] as List)) {
         listOfMovie.add(e);
+        filteredList.add(e);
       }
       if (uri.queryParameters.containsKey('id')) {
         String videoId = "${uri.queryParameters['id']}";
@@ -53,7 +94,6 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
             (uri.queryParameters['isMovie'] as String) == "true") {
           isMovie.value = true;
           // load Movie using id
-
           Map<String, dynamic> movieObject = (data['AllMovieDataList'] as List)
               .firstWhere((element) => (element as Map)['id'] == videoId,
                   orElse: () => {});
@@ -67,12 +107,14 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
               "${movieObject['s3']}",
               "${movieObject['s4']}"
             ];
+            hdArray.removeWhere((element) => element == "FALSE");
             List<String> nonHdArray = [
               "${movieObject['4s1']}",
               "${movieObject['4s2']}",
               "${movieObject['4s3']}",
               "${movieObject['4s4']}"
             ];
+            nonHdArray.removeWhere((element) => element == "FALSE");
             playableUrls.value = isHd == true
                 ? hdArray
                 : (movieObject.containsKey('480p') &&
@@ -90,7 +132,7 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
             hasDataFounded.value = false;
           }
         } else {
-          // load web series using Main-Id
+          hasDataFounded.value = false;
         }
       } else {
         hasDataFounded.value = false;
@@ -101,41 +143,7 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
   }
 
   setUpFlickManager(String url) {
-    // print("<><>asdsadas");
-    // onRewardedAdCompleted(() {
-    //   print('Rewarded ad completed!');
-    // });
-    //
-    // loadRewardedAd();
-
-    // Set up the ad completion listener
-    /* onRewardedAdCompleted(() {
-      videoPlayerController = VideoPlayerController.networkUrl(Uri.parse(url))
-        ..initialize()
-        ..addListener(
-          () {
-            // if (videoPlayerController?.value.hasError == true) {
-            //   hasDataFounded.value = false;
-            //   if (currentPlayerIndex.value < 4) {
-            //     currentPlayerIndex.value = currentPlayerIndex.value + 1;
-            //     videoPlayerController?.dispose();
-            //     setUpFlickManager(playableUrls[currentPlayerIndex.value]);
-            //     setState(() {});
-            //   }
-            // }
-          },
-        );
-
-      if (hasDataFounded.value) {
-        flickManager = FlickManager(
-            videoPlayerController: videoPlayerController!,
-            autoPlay: true,
-            autoInitialize: true);
-        dataManager = DataManager(flickManager: flickManager, urls: []);
-        videoIsLoaded.value = true;
-      }
-    });*/
-
+    print(url);
     player.open(Media(url)).then((value) {
       if (hasDataFounded.value) {
         videoIsLoaded.value = true;
@@ -158,31 +166,45 @@ class WebVideoPlayerState extends State<WebVideoPlayer> {
         }),
       ),
       endDrawer: Drawer(
-        child: Center(
-          child: Obx(() {
-            return ListView.builder(
-              itemBuilder: (context, index) => GestureDetector(
-                child: ListTile(
-                  title: Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Text(
-                        listOfMovie[index]['mn'],
-                        style: const TextStyle(color: Colors.white),
-                      )),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Search...',
+                  border: OutlineInputBorder(),
                 ),
-                onTap: () async {
-                  String movieUrl =
-                      'https://umeshkanhasoft.github.io/web_app/?id=${listOfMovie[index]['id']}&isMovie=true';
-                  js.context.callMethod('open', [movieUrl]);
-                },
+                onChanged: searchItems,
               ),
-              itemCount: listOfMovie.length,
-            );
-          }),
+            ),
+            Expanded(
+              child: Obx(() {
+                return ListView.builder(
+                  itemBuilder: (context, index) => GestureDetector(
+                    child: ListTile(
+                      title: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Text(
+                            filteredList[index]['mn'],
+                            style: const TextStyle(color: Colors.white),
+                          )),
+                    ),
+                    onTap: () async {
+                      String movieUrl =
+                          'https://umeshkanhasoft.github.io/web_app/?id=${filteredList[index]['id']}&isMovie=true';
+                      js.context.callMethod('open', [movieUrl]);
+                    },
+                  ),
+                  itemCount: filteredList.length,
+                );
+              }),
+            ),
+          ],
         ),
       ),
       body: Obx(() {
